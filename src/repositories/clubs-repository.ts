22be2c280d8da4aa3d202c61999a78a167da51b7
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-23 17:58:09 -0300

    feat: rota de listar clubs criada

---
+++ src/repositories/clubs-repository.ts
@@ -0,0 +1,14 @@
+import { ClubModel } from "../models/club-model";
+
+
+const database: ClubModel[] = [
+  {
+    id: 1,
+    name: "Real Madrid",
+    }
+]; 
+
+
+export const findAllClubs = async (): Promise<ClubModel[]> => {
+    return database;
+}
--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,4 +1,4 @@
-import { stat } from "fs";
+
 import { PlayerModel } from "../models/player-model";
 import * as PlayerRepository from "../repositories/players-repository";
 import * as HttpResponse from "../utils/http-helper";
